4d2bf85ce3666b5cc626ad890645a689f03455ea
TFolkner <markov.ilya.work@gmail.com>
2026-07-14 22:48:19 +0300

    Тестилки библиотек на плюсах

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -10,14 +10,25 @@ int dispatcher() {
 	init_data init{};
 	init.make_header();
 	init.disp_config();
-
+	
 	return 0;
 }
 
+int test() {
+	Eigen::Matrix<double, 3, 3> test_mtr = Eigen::Matrix<double, 3, 3>::Random();
+	test_mtr = test_mtr + test_mtr;
 
+	for (int row = 0; row < test_mtr.rows(); row++) {
+		for (int cols = 0; cols < test_mtr.cols(); cols++) {
+			std::cout << test_mtr(row, cols) << " ";
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
 
 int main() {
-	switch (dispatcher()) {
+	switch (test()) {
 	case 0:
 		break;
 
